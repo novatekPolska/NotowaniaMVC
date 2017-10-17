244d58ee955e786dffbaee3d869864830ff91136
szklarek <szklarek@SSZKLAREK>
2017-10-17 14:49:44 +0200

    Pageowanie grida

---
+++ NotowaniaMVC.Infrastructure/Database/Entities/XXX_R55_Deliveries.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotowaniaMVC.Infrastructure.Database.Entities
+{
+    public class XXX_R55_Deliveries
+    {
+        public virtual int Id { get; set; }
+        public virtual Guid Guid { get; set; }
+        public virtual decimal MinValue { get; set; }
+        public virtual decimal MaxValue { get; set; }
+    }
+}
